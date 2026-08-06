e30462aa5db289f77153143aa9e92fbdecddc6fd
JULIO CUBILLO PE�ARANDA <julio.cubillo.penaranda@est.una.ac.cr>
2026-08-06 17:23:19 -0600

    main con implementacion de clases

--- Sistema Gestor de Flujos de Trabajo.cpp
+++ Sistema Gestor de Flujos de Trabajo.cpp
@@ -1,8 +1,38 @@
-# include <iostream>
+#include <iostream>
+#include "ListaUsuarios.h"
+#include "GestorArchivos.h"
+#include "Sesion.h"
+
 using namespace std;
 
-
-// Main del programa
 int main() {
-  
+    // Cargar usuarios desde el archivo 
+    ListaUsuarios lista;
+    GestorArchivos::cargarUsuarios("usuarios.csv", lista);
+
+    // Solicitar credenciales al usuario
+    int id;
+    string contrasena;
+    cout << "Ingrese ID: ";
+    cin >> id;
+    cout << "Ingrese contraseña: ";
+    cin >> contrasena;
+
+    // Validar credenciales y manejar la sesión
+    if (lista.validarCredenciales(id, contrasena)) {
+        Usuario* u = lista.buscarUsuario(id);
+        Sesion sesion;
+        sesion.login(u);
+
+        cout << "Bienvenido " << u->getNombre() << endl;
+        if (sesion.esAdmin()) {
+            cout << "Menu: [1] Usuarios [2] Tareas [3] Kanban" << endl;
+        } else {
+            cout << "Menu: [1] Mis Tareas [2] Kanban" << endl;
+        }
+    } else {
+        cout << "Credenciales inválidas." << endl;
+    }
+
+    return 0;
 }
