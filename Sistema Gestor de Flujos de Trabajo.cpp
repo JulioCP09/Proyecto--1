4be34c97b7902e35039b982897c5b694c5e8f1a4
JULIO CUBILLO PE�ARANDA <julio.cubillo.penaranda@est.una.ac.cr>
2026-08-06 20:14:42 -0600

    implementacion del Menu principal

--- Sistema Gestor de Flujos de Trabajo.cpp
+++ Sistema Gestor de Flujos de Trabajo.cpp
@@ -6,6 +6,59 @@
 #include <iostream>
 using namespace std;
 
+// Funcion para mostrar el menu principal dependiendo del rol del usuario
+void menuPrincipal(Sesion& sesion, ListaUsuarios& lista) {
+    int opcion;
+    do {
+        cout << "\n--- MENU PRINCIPAL ---\n";
+        if (sesion.esAdmin()) { // Mostrar opciones para admin
+            cout << "1. Usuarios\n";
+            cout << "2. Tareas\n";
+            cout << "3. Kanban\n";
+        } else { // Mostrar opciones para usuario normal
+            cout << "1. Usuario\n";
+            cout << "2. Kanban\n";
+        }
+        cout << "0. Salir\n";
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
+        
+        // Dependiendo del rol del usuario, mostrar el menu correspondiente
+        if (sesion.esAdmin()) { // Menu para admin
+            switch (opcion) {
+            case 1:
+                MenuUsuarios::mostrarMenu(lista);
+                break;
+            case 2:
+                cout << "(Pendiente: implementar menu de tareas)\n";
+                break;
+            case 3:
+                cout << "(Pendiente: implementar tablero Kanban)\n";
+                break;
+            case 0:
+                cout << "Saliendo...\n";
+                break;
+            default:
+                cout << "Opcion invalida.\n";
+            }
+        } else { // Menu para usuario normal
+            switch (opcion) {
+            case 1:
+                MenuUsuariosNormales::mostrarMenu(lista, sesion.getUsuarioActual());
+                break;
+            case 2:
+                cout << "(Pendiente: implementar tablero Kanban)\n";
+                break;
+            case 0:
+                cout << "Saliendo...\n";
+                break;
+            default:
+                cout << "Opcion invalida.\n";
+            }
+        }
+    } while (opcion != 0);
+}
+
 int main() {
     // Cargar usuarios desde el archivo 
     ListaUsuarios lista;
@@ -26,13 +79,9 @@ int main() {
         sesion.login(u);
 
         cout << "Bienvenido " << u->getNombre() << endl;
-        if (sesion.esAdmin()) {
-            MenuUsuarios::mostrarMenu(lista);
-        } else {
-            MenuUsuariosNormales::mostrarMenu(lista, u);
-        }
+        menuPrincipal(sesion, lista);
     } else {
-        cout << "Credenciales inválidas." << endl;
+        cout << "Credenciales incorrectas.\n";
     }
 
     return 0;
